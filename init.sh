4be1f98cf411a24f5726940633e194a4fadb784c
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-06-04 01:03:50 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,7 +16,6 @@ if [ ! -s /etc/supervisor/conf.d/damon.conf ]; then
     WEB_PORT=${WEB_PORT:-'8008'}
   fi
   PRO_PORT=${PRO_PORT:-'80'}
-  CADDY_HTTP_PORT=2052
   WORK_DIR=/dashboard
   IS_UPDATE=${IS_UPDATE:-'no'}
   # 如不分离备份的 github 账户，默认与哪吒登陆的 github 账户一致
@@ -59,10 +58,6 @@ if [ ! -s /etc/supervisor/conf.d/damon.conf ]; then
    # 使用caddy反代
     GRPC_PROXY_RUN="$WORK_DIR/caddy run --config $WORK_DIR/Caddyfile --watch"
   cat > $WORK_DIR/Caddyfile  << EOF
-{
-    http_port $CADDY_HTTP_PORT
-}
-
 :$PRO_PORT {
     handle /${UUID} {
         file_server {
