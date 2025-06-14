45102125fea20ea88f9ebf67c0f6ed5bd2c2b83a
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-06-14 22:56:30 +0800

    Update backup.sh

--- template/backup.sh
+++ template/backup.sh
@@ -23,6 +23,38 @@ error() { echo -e "\033[31m\033[01m$*\033[0m" && exit 1; } # 红色
 info() { echo -e "\033[32m\033[01m$*\033[0m"; }   # 绿色
 hint() { echo -e "\033[33m\033[01m$*\033[0m"; }   # 黄色
 
+cmd_systemctl() {
+  local ENABLE_DISABLE=$1
+  if [ "$ENABLE_DISABLE" = 'enable' ]; then
+    if [ "$SYSTEM" = 'Alpine' ]; then
+      local TRY=5
+      until [ $(systemctl is-active nezha-dashboard) = 'active' ]; do
+        systemctl stop nezha-dashboard; sleep 1
+        systemctl start nezha-dashboard
+        ((TRY--))
+        [ "$TRY" = 0 ] && break
+      done
+      cat > /etc/local.d/nezha-dashboard.start << ABC
+#!/usr/bin/env bash
+
+systemctl start nezha-dashboard
+ABC
+      chmod +x /etc/local.d/nezha-dashboard.start
+      rc-update add local >/dev/null 2>&1
+    else
+      systemctl enable --now nezha-dashboard
+    fi
+
+  elif [ "$ENABLE_DISABLE" = 'disable' ]; then
+    if [ "$SYSTEM" = 'Alpine' ]; then
+      systemctl stop nezha-dashboard
+      rm -f /etc/local.d/nezha-dashboard.start
+    else
+      systemctl disable --now nezha-dashboard
+    fi
+  fi
+}
+
 # 运行备份脚本时，自锁一定时间以防 Github 缓存的原因导致数据马上被还原
 touch $(awk -F '=' '/NO_ACTION_FLAG/{print $2; exit}' $WORK_DIR/restore.sh)1
 
@@ -30,8 +62,15 @@ touch $(awk -F '=' '/NO_ACTION_FLAG/{print $2; exit}' $WORK_DIR/restore.sh)1
 [ "$1" = 'a' ] && WAY=Scheduled || WAY=Manualed
 [ "$1" = 'f' ] && WAY=Manualed && FORCE_UPDATE=true
 
-# 检查更新
+# 检查更新面板主程序 app 及 cloudflared
 cd $WORK_DIR
+DASHBOARD_NOW=$(./app -v)
+DASHBOARD_LATEST=$(wget -qO- "https://api.github.com/repos/naiba/nezha/releases/latest" | awk -F '"' '/"tag_name"/{print $4}')
+[[ "$DASHBOARD_LATEST" =~ ^v([0-9]{1,3}\.){2}[0-9]{1,3}$ && "$DASHBOARD_NOW" != "$DASHBOARD_LATEST" ]] && DASHBOARD_UPDATE=true
+
+CLOUDFLARED_NOW=$(./cloudflared -v | awk '{for (i=0; i<NF; i++) if ($i=="version") {print $(i+1)}}')
+CLOUDFLARED_LATEST=$(wget -qO- https://api.github.com/repos/cloudflare/cloudflared/releases/latest | awk -F '"' '/tag_name/{print $4}')
+[[ "$CLOUDFLARED_LATEST" =~ ^20[0-9]{2}\.[0-9]{1,2}\.[0-9]+$ && "$CLOUDFLARED_NOW" != "$CLOUDFLARED_LATEST" ]] && CLOUDFLARED_UPDATE=true
 
 # 检测是否有设置备份数据
 if [[ -n "$GH_REPO" && -n "$GH_BACKUP_USER" && -n "$GH_EMAIL" && -n "$GH_PAT" ]]; then
@@ -45,8 +84,62 @@ if [[ -n "$GH_REPO" && -n "$GH_BACKUP_USER" && -n "$GH_EMAIL" && -n "$GH_PAT" ]]
   fi
 fi
 
+# 分步骤处理
+if [[ "${DASHBOARD_UPDATE}${CLOUDFLARED_UPDATE}${IS_BACKUP}${FORCE_UPDATE}" =~ true ]]; then
+  # 更新面板和 resource
+  if [[ "${DASHBOARD_UPDATE}${FORCE_UPDATE}" =~ 'true' && "${IS_UPDATE}" == 'yes' ]]; then
+    hint "\n Renew dashboard app to $DASHBOARD_LATEST \n"
+    wget -O /tmp/dashboard.zip ${GH_PROXY}https://github.com/naiba/nezha/releases/download/$DASHBOARD_LATEST/dashboard-linux-$ARCH.zip
+    unzip /tmp/dashboard.zip -d /tmp
+    if [ -s /tmp/dist/dashboard-linux-$ARCH ]; then
+      info "\n Restart Nezha Dashboard \n"
+      if [ "$IS_DOCKER" = 1 ]; then
+        supervisorctl stop nezha >/dev/null 2>&1
+        sleep 10
+        mv -f /tmp/dist/dashboard-linux-$ARCH $WORK_DIR/app
+        supervisorctl start nezha >/dev/null 2>&1
+      else
+        cmd_systemctl disable >/dev/null 2>&1
+        sleep 10
+        mv -f /tmp/dist/dashboard-linux-$ARCH $WORK_DIR/app
+        cmd_systemctl enable >/dev/null 2>&1
+      fi
+      rm -rf /tmp/dist /tmp/dashboard.zip
+    fi
+   if [ -s /tmp/dashboard-linux-$ARCH ]; then
+      info "\n Restart Nezha Dashboard \n"
+      if [ "$IS_DOCKER" = 1 ]; then
+        supervisorctl stop nezha >/dev/null 2>&1
+        sleep 10
+        mv -f /tmp/dashboard-linux-$ARCH $WORK_DIR/app
+        supervisorctl start nezha >/dev/null 2>&1
+      else
+        cmd_systemctl disable >/dev/null 2>&1
+        sleep 10
+        mv -f /tmp/dashboard-linux-$ARCH $WORK_DIR/app
+        cmd_systemctl enable >/dev/null 2>&1
+      fi
+      rm -rf /tmp/dashboard.zip
+    fi   
+  fi
+
+
+
   # 克隆备份仓库，压缩备份文件，上传更新
   if [ "$IS_BACKUP" = 'true' ]; then
+    # 备份前先停掉面板，设置 git 环境变量，减少系统开支
+    if [ "$IS_DOCKER" != 1 ]; then
+      cmd_systemctl disable >/dev/null 2>&1
+      git config --global core.bigFileThreshold 1k
+      git config --global core.compression 0
+      git config --global advice.detachedHead false
+      git config --global pack.threads 1
+      git config --global pack.windowMemory 50m
+    else
+      supervisorctl stop nezha >/dev/null 2>&1
+    fi
+    sleep 10
+
     # 克隆现有备份库
     [ -d /tmp/$GH_REPO ] && rm -rf /tmp/$GH_REPO
     git clone https://$GH_PAT@github.com/$GH_BACKUP_USER/$GH_REPO.git --depth 1 --quiet /tmp/$GH_REPO
@@ -81,3 +174,12 @@ fi
       fi
     fi
   fi
+fi
+
+if [ "$IS_DOCKER" = 1 ]; then
+  supervisorctl start nezha >/dev/null 2>&1
+  [ $(supervisorctl status all | grep -c "RUNNING") = $(grep -c '\[program:.*\]' /etc/supervisor/conf.d/damon.conf) ] && info "\n All programs started! \n" || error "\n Failed to start program! \n"
+else
+  cmd_systemctl enable >/dev/null 2>&1
+  [ "$(systemctl is-active nezha-dashboard)" = 'active' ] && info "\n Nezha dashboard started! \n" || error "\n Failed to start Nezha dashboard! \n"
+fi
